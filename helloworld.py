74c66f3ad2d93c6b4a3204b5281747f23cf86cf9
acsiri <acsiri@gmail.com>
2026-04-24 15:42:17 +0530

    Add hello world Python application

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Hello World Application
+A simple Python program that greets the user.
+"""
+
+
+def hello_world():
+    """Print a friendly greeting message."""
+    print("Hello, World!")
+
+
+def main():
+    """Main entry point of the application."""
+    hello_world()
+
+
+if __name__ == "__main__":
+    main()
